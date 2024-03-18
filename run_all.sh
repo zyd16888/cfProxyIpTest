e3c9f2e626261ce88981c9f87c077a53d4377922
zyd16888 <zyd475182659@163.com>
2024-03-18 16:22:39 +0800

    fix err

--- run_all.sh
+++ run_all.sh
@@ -9,10 +9,10 @@ start_time=$(date +"%Y%m%d%H%M%S")
 
 bash run.sh
 
-github_token = "your github token"
-gist_id = "your gist id"
-file_path = "result_latest/ip-with-port.csv"
-file_path2 = "result_latest/baipiao.csv"
+github_token="your github token"
+gist_id="your gist id"
+file_path="result_latest/ip-with-port.csv"
+file_path2="result_latest/baipiao.csv"
 
 python3 update_gist.py -t "$github_token" -g "$gist_id" -f "$file_path"
 
