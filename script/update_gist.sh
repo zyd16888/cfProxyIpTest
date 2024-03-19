4f3f3425ffc922039b9c8174a0ee5e0bfe6728f7
zyd16888 <zyd475182659@163.com>
2024-03-19 13:46:07 +0800

    update script

--- script/update_gist.sh
+++ script/update_gist.sh
@@ -2,8 +2,8 @@
 
 github_token="your github token"
 gist_id="your gist id"
-file_path="../result_latest/ip-with-port.csv"
-file_path2="../result_latest/baipiao.csv"
+file_path="result_latest/ip-with-port.csv"
+file_path2="result_latest/baipiao.csv"
 
 python3 update_gist.py -t "$github_token" -g "$gist_id" -f "$file_path"
 
