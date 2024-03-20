cf82e717917a268401f90c442e93db4bb6d23df4
zyd16888 <zyd475182659@163.com>
2024-03-20 11:25:57 +0800

    update script

--- script/run_all.sh
+++ script/run_all.sh
@@ -5,8 +5,20 @@ start_time=$(date +"%Y%m%d%H%M%S")
 
 bash script/run.sh
 
-bash script/update_gist.sh
+export github_token="your github token"
+export gist_id="your gist id"
+
+update_msg=$(script/update_gist.sh)
 
 echo "脚本运行完成"
 end_time=$(date +"%Y%m%d%H%M%S")
 echo "脚本运行时间: $[$end_time-$start_time]秒"
+
+# sond message
+echo -e "$update_msg"
+
+# 设置环境变量
+export QYWX_AM=""
+
+
+python3 sendNotify.py -t "IP测速运行结果" -c "$update_msg \n\n脚本运行时间: $[$end_time-$start_time]秒"
--- script/update_gist.sh
+++ script/update_gist.sh
@@ -1,11 +1,39 @@
 #!/usr/bin/bash
 
-
-github_token="your github token"
-gist_id="your gist id"
 file_path="result_latest/ip-with-port.csv"
 file_path2="result_latest/baipiao.csv"
 
-python3 update_gist.py -t "$github_token" -g "$gist_id" -f "$file_path"
 
-python3 update_gist.py -t "$github_token" -g "$gist_id" -f "$file_path2"
+
+output1=$(python3 update_gist.py -t "$github_token" -g "$gist_id" -f "$file_path" -a "$area")
+
+output2=$(python3 update_gist.py -t "$github_token" -g "$gist_id" -f "$file_path2" -a "$area")
+
+update_msg="$area 文件上传情况："
+
+# 输出结果
+echo "output1 is $output1"
+echo "output2 is $output2"
+
+# 进行条件判断
+if [ "$output1" == "文件已成功更新到 Gist。" ]; then
+    # 处理成功的情况
+    echo "文件 ip-with-port 成功更新到 Gist。"
+    update_msg="$update_msg\n文件 ip-with-port 成功更新到 Gist。"
+else
+    # 处理失败的情况
+    echo "更新 Gist 失败。"
+    update_msg="$update_msg\n文件 ip-with-port 更新到 Gist 失败."
+fi
+
+if [ "$output2" == "文件已成功更新到 Gist。" ]; then
+    # 处理成功的情况
+    echo "文件 baipiao 成功更新到 Gist。"
+    update_msg="$update_msg\n文件 baipiao 成功更新到 Gist。"
+else
+    # 处理失败的情况
+    echo "更新 Gist 失败。"
+    update_msg="$update_msg\n文件 baipiao 更新到 Gist 失败."
+fi
+
+echo -e "$update_msg"
