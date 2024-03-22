ea8672ff0ef6027ff7e79a5ffe59ea073de64d94
zyd16888 <zyd475182659@163.com>
2024-03-22 11:46:48 +0800

    add send msg

--- other_script/run_all.sh
+++ other_script/run_all.sh
@@ -2,12 +2,28 @@
 
 cd /home/ubuntu/cfProxyIpTest/other_script
 
+start_time=$(date +"%Y%m%d%H%M%S")
+
 if [ ! -d logs ]; then
     mkdir logs
 fi
 
 bash run_iptest.sh >> logs/$(date +"%Y%m%d%H%M%S").log 2>&1
 
-bash run_format.sh ./result
+bash run_format.sh ./result/
+
+end_time=$(date +"%Y%m%d%H%M%S")
+
+echo "脚本运行时间: $[$end_time-$start_time]秒"
+
+# 设置环境变量
+export QYWX_AM=""
+
+# 判断环境变量是否为空
+if [ -n "$QYWX_AM" ]; then
+    # ./upx sync --strong ./result/
+    send_msg="$update_msg"$'\n\n'"脚本运行时间: $((end_time - start_time))秒"
+    python3 ../sendNotify.py -t "IP测速运行结果" -c "$send_msg"
+fi
 
 
