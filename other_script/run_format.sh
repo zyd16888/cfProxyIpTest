8ab81a258bf3c01d9cef1b248a514abff6e8ed40
zyd16888 <zyd475182659@163.com>
2024-03-22 13:19:08 +0800

    change debug

--- other_script/run_format.sh
+++ other_script/run_format.sh
@@ -9,22 +9,19 @@ fi
 # 获取传入的目录路径
 directory="$1"
 
-# 切换到指定目录
-cd "$directory" || exit 1
 
 # 遍历目录下的所有以 .csv 结尾的文件
-for file in *.csv; do
+for file in "$directory"/*.csv; do
     # 检查文件是否存在
     if [ -f "$file" ]; then
-        # 调用 Python 程序处理当前文件
-        python3 ../format-ipTest-result.py -i "$file" -o "format_${file}"
+        # 调用 Python 程序处理当前文件，并将输出放在当前目录下
+        python3 format-ipTest-result.py -i "$file" -o "${file%.csv}_formatted.csv"
     fi
 done
 
-cd ../
 
 if [ ! -d format_result ]; then
     mkdir format_result
 fi
 
-mv "$directory"/format_* format_result/
+mv "$directory"/*_formatted.csv format_result/
