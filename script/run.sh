c6bc835c54bc04fe0fb2eb522ee3533c7c485576
zyd16888 <zyd475182659@163.com>
2024-03-26 21:48:25 +0800

    debug

--- script/run.sh
+++ script/run.sh
@@ -26,7 +26,7 @@ fi
 
 # 获取 baipiaoIP
 echo "开始获取 baipiaoIP"
-wget -O baipiao.zip https://baipiao.test.upcdn.net || { echo "Error: Failed to download baipiao.zip." >&2; exit 1; }
+wget -O baipiao.zip http://baipiao.test.upcdn.net || { echo "Error: Failed to download baipiao.zip." >&2; exit 1; }
 unzip -o baipiao.zip -d baipiao || { echo "Error: Failed to unzip baipiao.zip." >&2; exit 1; }
 
 # 格式化
@@ -43,7 +43,7 @@ rm -rf baipiao
 
 count=$(wc -l < baipiao.csv)
 echo "有效IP数量为 $count 个"
-export run_msg="$run_msg\n有效IP数量为 $count 个\n"
+export run_msg="$run_msg"$'\n'"baipiao 有效IP数量为 $count 个"$'\n'
 
 # 备份 baipiao 文件
 if [ ! -d bak_baipiao ]; then
@@ -65,7 +65,7 @@ rm proxy.txt raw_ip.txt
 
 count=$(wc -l < ip.txt)
 echo "共获取到 $count 个IP"
-export run_msg="$run_msg\nip-with-port共获取到 $count 个IP\n "
+export run_msg="$run_msg"$'\n'"ip-with-port共获取到 $count 个IP"$'\n'
 
 # 添加端口号
 python3 ip-add-port.py -i ip.txt -o ip-with-port.txt -p 443 || { echo "Error: Failed to add port to IP addresses." >&2; exit 1; }
@@ -76,7 +76,7 @@ rm ip-with-port.txt
 
 count=$(wc -l < ip-with-port.csv)
 echo "ip-with-port有效IP数量为 $count 个"
-run_msg="$run_msg\nip-with-port有效IP数量为 $count 个\n"
+run_msg="$run_msg"$'\n'"ip-with-port有效IP数量为 $count 个"$'\n'
 
 echo -e "$run_msg" > npipe &
 
